8173b7c854f80feff59722ebc6f739e8b6b3359a
timolxl <timo2503k@gmail.com>
2018-02-20 12:07:39 +0100

    final commit

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   var license = 'license-string' // <-- Please replace this with the content of your license file. The JSON-object must be in string format.
   var container = document.getElementById('pesdk')
-
+  
   var editor = new PhotoEditorSDK.UI.ReactUI({
     container: container,
     license: license,
